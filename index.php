1d9bc9ef276c12def310a0f97452bb982da5d6be
Harrison Taylor <taylorh23@cardiff.ac.uk>
2017-08-25 16:56:54 +0100

    recreated logos in inkscape to be sharper - added favicon

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
   include("./includes/header.htm");
 ?>
 
-<div class="ui two column centered grid">
+<div class="ui two column stackable centered grid">
   <div class="column">
     <img id="cdf-logo" class="ui centered fluid tiny  image" src="./img/cdf_logo.jpg">
     <div class="ui divider"></div>
     <img id="compsoc-logo" class="ui centered fluid large image" src="./img/logo.png">
     <h1 class="ui centered header">
-      Welcome to Cardiff University's Computer Science Society
+      Welcome&nbsp;to&nbsp;Cardiff University's Computer&nbsp;Science Society
     </h1>
     <div class="ui horizontal divider">
       <button class="ui large red button">Become a member</button>
